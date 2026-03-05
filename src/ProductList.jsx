eec53ca123e5494deab68d2e481ada1d9e08d96d
Eric <ewbservices76@gmail.com>
2026-03-04 21:51:34 -0500

    updates

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -252,6 +252,7 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -274,8 +275,27 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-                        
-
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1>
+                                <div>{category.category}</div>
+                            </h1>
+                        <div className="product-list">
+                            {category.plants.map((plant, plantIndex) =>(
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image"
+                                        src={plant.image}
+                                        alt={plant.name}/>
+                                        <div className="product-title">{plant.name}</div>
+                                        <div className="product-description">{plant.description}</div>
+                                        <div className="product-cost">{plant.cost}</div>
+                                        <button className="product-button"
+                                                onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                </div>
+                            ))}
+                        </div>
+                        </div>
+                    ))}    
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
